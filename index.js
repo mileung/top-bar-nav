ec4d06abba18837c1849a3e8282dc4105b1855e8
MiLeung <michaeleleung@gmail.com>
2018-07-06 19:42:13 -0700

    1.1.1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /** @format */
 import TopBarNav from "./src/TopBarNav";
-import { AppRegistry } from "react-native";
-import Example from "./src/Example";
-import { name } from "./app.json";
-
-AppRegistry.registerComponent(name, () => Example);
+// import { AppRegistry } from "react-native";
+// import Example from "./src/Example";
+// import { name } from "./app.json";
+//
+// AppRegistry.registerComponent(name, () => Example);
 
 export default TopBarNav;
